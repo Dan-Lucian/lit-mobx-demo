2b3f0dd798735edcf7bb9ee17e99c61efd813cb7
Gaina Dan <danugaina1403@gmail.com>
2022-08-05 11:46:52 +0300

    [feat] add /mobx route

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -7,11 +7,13 @@ import '../pages/page-index.js';
 import '../pages/page-login.js';
 import '../pages/page-about.js';
 import '../pages/page-404.js';
+import '../pages/page-mobx.js';
 
 const PageIndex: TemplateResult<1> = html`<page-index></page-index>`;
 const PageLogin: TemplateResult<1> = html`<page-login></page-login>`;
 const PageAbout: TemplateResult<1> = html`<page-about></page-about>`;
 const Page404: TemplateResult<1> = html`<page-404></page-404>`;
+const PageMobx: TemplateResult<1> = html`<page-mobx></page-mobx>`;
 
 const routes: IRoute[] = [
   {
@@ -27,6 +29,11 @@ const routes: IRoute[] = [
   {
     path: '/about',
     component: PageAbout,
+    roles: [EnumRoleUser.Employee, EnumRoleUser.Manager],
+  },
+  {
+    path: '/mobx',
+    component: PageMobx,
     roles: [EnumRoleUser.Employee, EnumRoleUser.Manager],
   },
   {
---
+++ src/pages/page-mobx.ts
@@ -0,0 +1,15 @@
+import { html, LitElement, TemplateResult } from 'lit';
+import { customElement } from 'lit/decorators/custom-element.js';
+
+import '../components/link-router.js';
+
+@customElement('page-mobx')
+export class PageMobx extends LitElement {
+  protected render(): TemplateResult<1> {
+    return html`
+      <main>
+        <h1>Page Mobx</h1>
+      </main>
+    `;
+  }
+}
